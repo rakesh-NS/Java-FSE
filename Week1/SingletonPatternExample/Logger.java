5ab0d26cf5c907f3e8eb953b18b357d17ced5478
rakesh-NS <rakeshrk00745@gmail.com>
2026-06-18 20:36:33 +0530

    Added Week1 Singleton and Factory Method Pattern examples

---
+++ Week1/SingletonPatternExample/Logger.java
@@ -0,0 +1,15 @@
+class Logger {
+
+    private static Logger instance = new Logger();
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
